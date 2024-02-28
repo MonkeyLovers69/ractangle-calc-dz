8d7a5a3f94d096dd5a6cbf4de139d21fd1957046
MonkeyLovers69 <137757068+MonkeyLovers69@users.noreply.github.com>
2024-02-28 11:53:39 +0500

    Add files via upload

---
+++ dz-ractangle/dz-ractangle/dz-ractangle.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int width;
+    int height;
+    int choice;
+    cout << "hello! type 1 if you want to know your ractangle area and perimeter: "<<endl;
+    cin >> choice;
+    if (choice == 1)
+    {
+        while (choice ==1 )
+        {
+            cout << "enter height: ";
+            cin >> height;
+            cout << endl;
+            cout << "enter width: ";
+            cin >> width;
+            cout << endl << height * width<<endl;
+            cout << "do you want repeat? type 1 if yes, type 2 if no: ";
+            cin >> choice;
+            system("cls");
+        }
+     }
+    else
+    {
+        return(0);
+    }
+}
